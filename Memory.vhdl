1f73a6fec0a208b4e5138ecb3cd72e8d2542d44d
jujhaar2409 <jujhaar.singh.123@gmail.com>
2022-05-09 18:27:51 +0530

    fixing memory forwarding

--- Memory.vhdl
+++ Memory.vhdl
@@ -58,8 +58,10 @@ architecture behav of IM is
 	type vec_array is array(0 to 2**5 - 1) of std_logic_vector(15 downto 0);
 	signal RAM: vec_array:= (
 		-- 0 => "0111000000001010",
-		0 => "0111001000000000",
-		1 => "0111000001000000",
+		0 => "0111001000000000", -- lw r1, r0, 0, load mem 0 in r1
+		1 => "0111000000000001", -- lw r0, r0, 1, load mem 1 in r0
+		2 => "0001001000000000", -- add r0, r0, r1, add r1 and r0 and store in r0
+
 		-- 1 => "0001001011011000", -- r1, r3, r3
 		-- 2 => "0000000000000001", -- r0, 1
 		-- 3 => "0001001011111000", -- r1, r3, r7
